7e09b0cb6b6d7345138625695ef0db80b4dd1853
yuuki1293 <yuuki1293@outlook.jp>
2022-03-27 18:50:32 +0900

    Create: Time.scala

---
+++ src/main/java/com/staticallytyped/kagawaassist/coordinate/f3/Time.scala
@@ -0,0 +1,7 @@
+package com.staticallytyped.kagawaassist.coordinate.f3
+
+import com.mojang.blaze3d.matrix.MatrixStack
+
+class Time(column: Int, f3: F3)(implicit matrixStack: MatrixStack) extends AbstractPart(f3){
+
+}
