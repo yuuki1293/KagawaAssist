09a333e496a681cf66b2fa75507939faac8a7c22
yuuki1293 <yuuki1293@outlook.jp>
2022-04-12 23:28:16 +0900

    各値をConfigオブジェクトから読み取るようにした

--- src/main/scala/staticallytyped/kagawaassist/f3/Time.scala
+++ src/main/scala/staticallytyped/kagawaassist/f3/Time.scala
@@ -1,15 +1,18 @@
 package staticallytyped.kagawaassist.f3
 
 import com.mojang.blaze3d.matrix.MatrixStack
+import staticallytyped.kagawaassist.Config
 
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class Time(column: Int, f3: F3)(implicit matrixStack: MatrixStack) extends AbstractPart(column)(f3){
   override def render(): Unit = {
-    //hh.mm.ss
-    val time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
-    drawText("time: ", f3.textColor)
-    drawText(time, f3.valueColor)
+    if(Config.displayTime.get()) {
+      val timePattern = Config.timePattern.get()
+      val time = LocalTime.now().format(DateTimeFormatter.ofPattern(timePattern))
+      drawText("time: ", f3.textColor)
+      drawText(time, f3.valueColor)
+    }
   }
 }
