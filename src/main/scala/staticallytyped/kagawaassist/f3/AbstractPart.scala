3be6f31a74e37c71cbf05c39d5e04f44715f974d
yuuki1293 <yuuki1293@outlook.jp>
2022-08-19 23:49:49 +0900

    update: draw

--- src/main/scala/staticallytyped/kagawaassist/f3/AbstractPart.scala
+++ src/main/scala/staticallytyped/kagawaassist/f3/AbstractPart.scala
@@ -4,7 +4,13 @@ import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue
 
 abstract class AbstractPart(val f3: F3)(implicit poseStack: PoseStack) {
-  val draw: (String, ConfigValue[Int]) => Unit = f3.drawText.draw
+  def draw(text: String, color: Int): Unit = {
+    f3.drawText.draw(text, color)
+  }
+
+  def draw(text: String, color: ConfigValue[Int]): Unit = {
+    f3.drawText.draw(text, color)
+  }
 
   var cancel = false
 
