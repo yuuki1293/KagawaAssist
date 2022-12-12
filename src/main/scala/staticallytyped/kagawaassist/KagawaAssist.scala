51f6eba03938d48c9be27b53403002f970f1ab41
yuuki1293 <yuuki1293@outlook.jp>
2022-12-12 16:08:27 +0900

    fix: error

--- src/main/scala/staticallytyped/kagawaassist/KagawaAssist.scala
+++ src/main/scala/staticallytyped/kagawaassist/KagawaAssist.scala
@@ -1,4 +1,4 @@
-package staticallytyped.kagawaassist;
+package staticallytyped.kagawaassist
 
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.ModLoadingContext
@@ -7,20 +7,25 @@ import net.minecraftforge.fml.config.ModConfig
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import org.apache.logging.log4j.LogManager
-import staticallytyped.kagawaassist.f3.F3;
+import staticallytyped.kagawaassist.KagawaAssist.doClientStuff
+import staticallytyped.kagawaassist.f3.F3
+
+import scala.annotation.unused
 
 @Mod(KagawaAssist.MODID)
-object KagawaAssist {
-  final val MODID = "kagawaassist"
-  final val LOGGER = LogManager.getLogger()
-
+class KagawaAssist {
   FMLJavaModLoadingContext.get().getModEventBus.addListener(doClientStuff)
 
   ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.spec, "KagawaAssist.toml")
   // Register ourselves for server and other game events we are interested in
   MinecraftForge.EVENT_BUS.register(this)
+}
 
-  private def doClientStuff(event: FMLClientSetupEvent): Unit = {
+object KagawaAssist {
+  final val MODID = "kagawaassist"
+  final val LOGGER = LogManager.getLogger()
+
+  private def doClientStuff(@unused event: FMLClientSetupEvent): Unit = {
     MinecraftForge.EVENT_BUS.register(new F3())
   }
 }
