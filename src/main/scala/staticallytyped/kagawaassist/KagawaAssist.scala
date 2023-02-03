61534e2d21aaf7da573fd0adb337fabd09685e13
yuuki1293 <yuuki1293@outlook.jp>
2023-02-03 23:54:44 +0900

    feat: register settings screen

--- src/main/scala/staticallytyped/kagawaassist/KagawaAssist.scala
+++ src/main/scala/staticallytyped/kagawaassist/KagawaAssist.scala
@@ -1,17 +1,22 @@
 package staticallytyped.kagawaassist
 
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.screen.Screen
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.config.ModConfig
+import net.minecraftforge.fml.{ExtensionPoint, ModLoadingContext}
 import org.apache.logging.log4j.{LogManager, Logger}
 import staticallytyped.kagawaassist.chat.Chat
 import staticallytyped.kagawaassist.f3.F3
+import staticallytyped.kagawaassist.gui.screen.ModSettingsScreen
 
 @Mod(KagawaAssist.MODID)
 class KagawaAssist {
   ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.spec, "KagawaAssist.toml")
+  ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY,
+    () => (mc: Minecraft, screen: Screen) => new ModSettingsScreen(screen)
+  )
 
   MinecraftForge.EVENT_BUS.register(this)
   MinecraftForge.EVENT_BUS.register(F3)
